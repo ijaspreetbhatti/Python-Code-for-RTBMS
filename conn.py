14e2ecc96debc356643c229725000778d25a7a08
b7ibmjava05@iiht.tech <b7ibmjava05@iiht.tech>
2019-11-05 09:05:12 +0530

    dded random Data

--- conn.py
+++ conn.py
@@ -3,6 +3,7 @@ import pymysql
 mydb = pymysql.connect(host='localhost', user='root', passwd='', db='examplepy')
 name = "Jas"
 job = "Dev"
+random = "RandomData"
 cursor = mydb.cursor()
 cursor.execute("INSERT INTO emp(name, job) VALUES(%s, %s)", (name, job))
 mydb.commit()
